f7af4f497d8979046ec81563a3ac2f1f7018deac
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-02-01 03:28:16 -0500

    update next.config.js to accepts photo domains

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    domains: [
+      's3-media1.fl.yelpcdn.com',
+      's3-media2.fl.yelpcdn.com',
+      's3-media3.fl.yelpcdn.com',
+      's3-media4.fl.yelpcdn.com',
+    ],
+  },
 };
