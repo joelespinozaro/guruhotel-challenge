7816e072fd5c5fe665a49497017458e46a07426e
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-02-01 15:31:44 -0500

    Add link to BusinessDetails Component

--- components/Business.js
+++ components/Business.js
@@ -1,11 +1,14 @@
 import Image from 'next/Image';
-import EyeIcon from './Icons/EyeIcon';
-import PhoneIcon from './Icons/PhoneIcon';
+import Link from 'next/link';
+
 import DirectionIcon from './Icons/DirectionIcon';
+import PhoneIcon from './Icons/PhoneIcon';
+import EyeIcon from './Icons/EyeIcon';
 import Rating from './Rating';
 
 export default function Business({ business }) {
   const {
+    id,
     name,
     photos,
     location,
@@ -15,47 +18,49 @@ export default function Business({ business }) {
     isViewed,
   } = business;
   return (
-    <section className="list-wrap-item">
-      <div className="business-wrap__img-wrapper">
-        <Image
-          width={140}
-          height={140}
-          objectFit="cover"
-          layout="intrinsic"
-          className="business-wrap__img-wrapper--img"
-          src={photos[0]}
-          alt={name}
-        />
-      </div>
-      <div className="business-wrap__body">
-        {isViewed && (
-          <EyeIcon
-            className="business-wrap__body--viewed"
-            width={22}
-            height={22}
-            fill="#bbb"
+    <Link href={`/business/${encodeURIComponent(id)}`}>
+      <section className="list-wrap-item">
+        <div className="business-wrap__img-wrapper">
+          <Image
+            width={140}
+            height={140}
+            objectFit="cover"
+            layout="intrinsic"
+            className="business-wrap__img-wrapper--img"
+            src={photos[0]}
+            alt={name}
           />
-        )}
-        <h2 className="business-wrap__body--title">{name}</h2>
-        <div className="business-wrap__body--phone">
-          <div>
-            <PhoneIcon width={15} height={15} />
-          </div>
-          <span>{phone ? phone : 'phone not registered'}</span>
         </div>
-        <address className="business-wrap__body--address">
-          <div>
-            <DirectionIcon width={15} height={15} />
+        <div className="business-wrap__body">
+          {isViewed && (
+            <EyeIcon
+              className="business-wrap__body--viewed"
+              width={22}
+              height={22}
+              fill="#bbb"
+            />
+          )}
+          <h2 className="business-wrap__body--title">{name}</h2>
+          <div className="business-wrap__body--phone">
+            <div>
+              <PhoneIcon width={15} height={15} />
+            </div>
+            <span>{phone ? phone : 'phone not registered'}</span>
           </div>
-          <span> {location.formatted_address}</span>
-        </address>
-        <div className="business-wrap__body--raiting">
-          <div>
-            <Rating ratingValue={rating} />
+          <address className="business-wrap__body--address">
+            <div>
+              <DirectionIcon width={15} height={15} />
+            </div>
+            <span> {location.formatted_address}</span>
+          </address>
+          <div className="business-wrap__body--raiting">
+            <div>
+              <Rating ratingValue={rating} />
+            </div>
+            <span>({review_count})</span>
           </div>
-          <span>({review_count})</span>
         </div>
-      </div>
-    </section>
+      </section>
+    </Link>
   );
 }
