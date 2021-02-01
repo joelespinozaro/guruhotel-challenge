ccf2680174d361d9c81390fc1a3bf627749625b0
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-02-01 03:44:34 -0500

    Add Head Component in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import store from '../store';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Head>
+        <title>GuruHotel Challenge</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
