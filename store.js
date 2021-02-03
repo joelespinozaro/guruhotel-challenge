b26f159eb0b23293f3d26229aed84f2e60726693
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-02-03 00:39:19 -0500

    Update env variables calls

--- store.js
+++ store.js
@@ -1,16 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './redux/reducers';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
 
 const composeEnhancers =
-  (typeof window != 'undefined' &&
+  (typeof window != "undefined" &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 const middlewares = [thunk];
 
 const enhancers =
-  process.env.ENV !== 'production'
+  process.env.NEXT_PUBLIC_ENV !== "production"
     ? composeEnhancers(applyMiddleware(...middlewares))
     : applyMiddleware(...middlewares);
 
